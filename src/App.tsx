e309830be510561285f2a68d63897d50a2a950ff
Jacob Handwerger <jstudyeth@gmail.com>
2023-08-09 12:22:08 -0400

    filter v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Url } from "url";
 
 interface User {
   name: {
@@ -24,6 +23,7 @@ interface User {
 
 const App = () => {
   const [ userData, setUserData ] = useState<User[]>([]);
+  const [ search, setSearch ] = useState("");
   
   const fetchData = async () => {
     try {
@@ -34,27 +34,48 @@ const App = () => {
     }
   }
 
+  const handleFilter = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!search.length) {
+      setUserData(userData);
+      return;
+    }
+    const filteredUsers = userData.filter(user => user.name.first.toLowerCase().includes(search.toLowerCase()));
+    setUserData(filteredUsers);
+    console.log("Filtered Users: ", filteredUsers);
+    console.log("Search: ", search);
+  }
+
   useEffect(() => {
     fetchData();
   }, [])
+
   return (
-    <div id="user-grid">
-      {
-        userData.map((e, i) => {
-          return (
-            <div key={i} className="user-card">
-              <p><span>First Name:</span> {e.name.first}</p>
-              <p><span>Last Name:</span> {e.name.last}</p>
-              <img src={e.picture.medium} alt="pfp" />
-              <p><span>Email:</span> {e.email}</p>
-              <p><span>Address:</span> {e.location.street.number} {e.location.street.name}</p>
-              <p>{e.location.city}, {e.location.state}</p>
-              <p>{e.location.country}</p>
-             </div>
-          )
-        })
-      }
-    </div>
+    <>
+      <div className="filter">
+        <form onSubmit={handleFilter}>
+          <input type="text" value={search} onChange={(e)=>setSearch(e.target.value)}></input>
+          <button type="submit">Search</button>
+        </form>
+      </div>
+      <div id="user-grid">
+        {
+          userData.map((e, i) => {
+            return (
+              <div key={i} className="user-card">
+                <p><span>First Name:</span> {e.name?.first}</p>
+                <p><span>Last Name:</span> {e.name.last}</p>
+                <img src={e.picture.medium} alt="pfp" />
+                <p><span>Email:</span> {e.email}</p>
+                <p><span>Address:</span> {e.location.street.number} {e.location.street.name}</p>
+                <p>{e.location.city}, {e.location.state}</p>
+                <p>{e.location.country}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+    </>
   );
 }
 
